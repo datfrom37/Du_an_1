9e055bcf56c0be9c3d19b6dbe3be16fd7f26a51b
BiUsagi <tubanovel@gmail.com>
2023-11-07 17:06:20 +0700

    123

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php include './layouts/header.php'; ?>
 
-<?php switch ($_GET["action"]) {
-    case 'contact':
-        include './pages/contact.php';
-        break;
-    case 'about':
-        include './pages/about.php';
-        break;
+<?php //switch ($_GET["action"]) {
+//     case 'contact':
+//         include './pages/contact.php';
+//         break;
+//     case 'about':
+//         include './pages/about.php';
+//         break;
     
-    default:
-        include './pages/home.php';
-        break;
-}
+//     default:
+//         include './pages/home.php';
+//         break;
+// }
 ?>
+
+
+
+
+
 
 <?php include './layouts/footer.php' ?>
