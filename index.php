d2ae62c4a5780f39bd1b7d219f63819614a0d0e2
BiUsagi <tubanovel@gmail.com>
2023-11-07 19:13:52 +0700

    123

--- index.php
+++ index.php
@@ -45,19 +45,11 @@
             
         </div>
     </header>
-    <!-- <div class="jumbotron text-white" style="height: 400px; overflow: hidden;">
+    <div class="jumbotron text-white" style="height: 400px; overflow: hidden;">
         <div class="container" style="max-width: 100%; max-height: 100%; overflow: hidden;">
             <img src="./images/banner1.jpg" alt="Banner" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
-    </div> -->
-
-
-
-
-
-
-
-
+    </div>
 
 
 
@@ -70,50 +62,6 @@
 
 
     
-        <div class="container text-left mt-4">
-            <h2>ẨM THỰC</h2>
-        </div>
-        
-        <!-- Phần hiển thị hình ảnh -->
-        <div class="container-fluid bg-light">
-            <div class="row">
-                <div class="col-md-1">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
-                </div>
-                <div class="col-md-1">
-                </div>
-                
-            </div>
-        </div>
 
 
 
--- pages/home.php
+++ pages/home.php
@@ -1,25 +1,45 @@
-<main>
-    <div class="container my-5">
-        <div class="bg-body-tertiary p-5 rounded">
-            <div class="col-sm-8 py-5 mx-auto">
-                <h1 class="display-5 fw-normal">Home Page</h1>
-                <p class="fs-5">This example shows how responsive
-                    offcanvas menus work within the navbar. For
-                    positioning of navbars, checkout the <a
-                        href="/docs/5.3/examples/navbar-static/">top</a>
-                    and <a href="/docs/5.3/examples/navbar-fixed/">fixed
-                        top</a> examples.</p>
-                <p>From the top down, you'll see a dark navbar, light
-                    navbar and a responsive navbar—each with offcanvases
-                    built in. Resize your browser window to the large
-                    breakpoint to see the toggle for the offcanvas.</p>
-                <p>
-                    <a class="btn btn-primary"
-                        href="/docs/5.3/components/navbar/#offcanvas"
-                        role="button">Learn more about offcanvas navbars
-                        »</a>
-                </p>
+
+<div class="container text-left mt-4">
+            <h2>ẨM THỰC</h2>
+        </div>
+        
+        <!-- Phần hiển thị hình ảnh -->
+        <div class="container-fluid bg-light">
+            <div class="row">
+                <div class="col-md-1">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                    <img src="./images/logo1.jpg" alt="Ảnh 1" class="rounded-image">
+                </div>
+                <div class="col-md-1">
+                </div>
+                
             </div>
         </div>
-    </div>
-</main>
