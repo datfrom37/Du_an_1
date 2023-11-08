319dbb0d166fbd0e654454a4967ae51e6f4c089f
BiUsagi <tubanovel@gmail.com>
2023-11-08 12:49:11 +0700

    12331

--- layouts/header.php
+++ layouts/header.php
@@ -8,7 +8,58 @@
             href="./public/bootstrap-5.3.2/dist/css/bootstrap.css">
         <script src="./public/bootstrap-5.3.2/dist/js/bootstrap.js"></script>
          <!-- Sử dụng thư viện Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" 
+         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" 
+         crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="../public/css/index.css">
     </head>
     <body>
+    <header class="main-header-two">
+            <div class="main-header-two__top">
+                <div class="container">
+                    <div class="main-header-two__top-inner">
+                        <div class="main-header-two__logo">
+                            <a href="index.php"><img src="./images/logo1.jpg" alt="" width="80"></a>
+                        </div>
+                        <div class="main-header-two__right">
+                            <ul class="list-unstyled main-header-two__contact-list">
+                                <li class="contact-item">
+                                    <div class="icon">
+                                        <i class="fas fa-mobile"></i>
+                                    </div>
+                                    <div class="text">
+                                        <a href="tel:923076806860">+ 84 343 561 287</a>
+                                    </div>
+                                </li>
+                                <li class="contact-item">
+                                    <div class="icon">
+                                        <i class="fas fa-envelope-open"></i>
+                                    </div>
+                                    <div class="text">
+                                        <a href="mailto:tubanovel@gmail.com">tubanovel@gmail.com</a>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div class="main-header-two__search-cart-box">
+                                <div class="main-header-two__search-form-box">
+                                    <form class="main-header-two__search-form mc-form" data-url="MC_FORM_URL"
+                                        novalidate="novalidate">
+                                        <div class="main-header-two__search-input-box">
+                                            <div class="main-header-two__search-icon"><i class="fas fa-search fa-lg" style='margin-bottom: 0.5vh;'></i></div>
+                                            <input type="search" placeholder="Search here" name="search">
+                                            <button type="submit"
+                                                class="main-header-two__search-btn thm-btn"> Search</button>
+                                        </div>
+                                    </form>
+                                    <div class="mc-form__response"></div>
+                                </div>
+                                <div class="main-header-two__cart-box">
+                                    <a href="menu.html" class="main-header-two__cart fas fa-bars"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </header>
+    
