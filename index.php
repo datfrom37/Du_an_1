a50dfc57d679e2a0699e72c14d72d99f97e80464
datfrom37 <luongtiendat195@gmail.com>
2023-11-22 12:49:53 +0700

    dat

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
-<?php include './config/connect.php';
+<?php 
+      session_start();
+      include './config/connect.php';
       include './dao/congthucDAO.php';
       include './dao/loaiDAO.php';
       include "dao/pdo.php";
+      include './dao/khachhangDAO.php';
 ?>
 
 
