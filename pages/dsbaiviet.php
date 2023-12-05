76f7fffcba04ccd811578b060536559eb3734f48
BiUsagi <tubanovel@gmail.com>
2023-12-05 20:15:32 +0700

    123

--- pages/dsbaiviet.php
+++ pages/dsbaiviet.php
@@ -9,7 +9,7 @@
                 <h2 class="section-title__title">TIN TỨC & BÀI VIẾT</h2>
             </div> -->
             <div class="product__showing-text-box">
-                <p class="product__showing-text mr-bot-new">Hiện thị 1–6 trên 12 bài viết</p>
+                <p class="product__showing-text mr-bot-new">Hiện thị 1–9 trên 12 bài viết</p>
             </div>
             <div class="row">
                 <!--News One Single Start-->
@@ -30,6 +30,108 @@
                                 </li>
                             </ul>
                             <h3 class="news-one__title"><a href="news-details.html">Which healthy food growth
+                                    strategies required</a></h3>
+                            <div class="news-one__bottom">
+                                <div class="news-one__read-more">
+                                    <a href="news-details.html">Read More</a>
+                                </div>
+                                <div class="news-one__comment">
+                                    <a href="news-details.html"><i class="fas fa-comments"></i>02</a>
+                                </div>
+                            </div>
+                            <div class="news-one__date">
+                                <p>20 Sep</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--News One Single End-->
+                <!--News One Single Start-->
+                <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="200ms">
+                    <div class="news-one__single">
+                        <div class="news-one__img-box">
+                            <div class="news-one__img">
+                                <img src="/images/banner 3.png" alt="">
+                            </div>
+                        </div>
+                        <div class="news-one__content-box">
+                            <ul class="news-one__meta list-unstyled">
+                                <li>
+                                    <a href="news-details.html"><i class="fa fa-tag"></i>Apartment</a>
+                                </li>
+                                <li>
+                                    <a href="news-details.html"><i class="fas fa-user-circle"></i>by Admin</a>
+                                </li>
+                            </ul>
+                            <h3 class="news-one__title"><a href="news-details.html">Donec aliquet blandit enim
+                                    feugiat Suspendisse</a></h3>
+                            <div class="news-one__bottom">
+                                <div class="news-one__read-more">
+                                    <a href="news-details.html">Read More</a>
+                                </div>
+                                <div class="news-one__comment">
+                                    <a href="news-details.html"><i class="fas fa-comments"></i>02</a>
+                                </div>
+                            </div>
+                            <div class="news-one__date">
+                                <p>20 Sep</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--News One Single End-->
+                <!--News One Single Start-->
+                <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="300ms">
+                    <div class="news-one__single">
+                        <div class="news-one__img-box">
+                            <div class="news-one__img">
+                                <img src="/images/banner 4.png" alt="">
+                            </div>
+                        </div>
+                        <div class="news-one__content-box">
+                            <ul class="news-one__meta list-unstyled">
+                                <li>
+                                    <a href="news-details.html"><i class="fa fa-tag"></i>Apartment</a>
+                                </li>
+                                <li>
+                                    <a href="news-details.html"><i class="fas fa-user-circle"></i>by Admin</a>
+                                </li>
+                            </ul>
+                            <h3 class="news-one__title"><a href="news-details.html">Luctus sit amet eu nibh tempus
+                                    turpis.</a></h3>
+                            <div class="news-one__bottom">
+                                <div class="news-one__read-more">
+                                    <a href="news-details.html">Read More</a>
+                                </div>
+                                <div class="news-one__comment">
+                                    <a href="news-details.html"><i class="fas fa-comments"></i>02</a>
+                                </div>
+                            </div>
+                            <div class="news-one__date">
+                                <p>20 Sep</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--News One Single End-->
+                <!--News One Single Start-->
+                <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="400ms">
+                    <div class="news-one__single">
+                        <div class="news-one__img-box">
+                            <div class="news-one__img">
+                                <img src="/images/banner 5.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="news-one__content-box">
+                            <ul class="news-one__meta list-unstyled">
+                                <li>
+                                    <a href="news-details.html"><i class="fa fa-tag"></i>Apartment</a>
+                                </li>
+                                <li>
+                                    <a href="news-details.html"><i class="fas fa-user-circle"></i>by Admin</a>
+                                </li>
+                            </ul>
+                            <h3 class="news-one__title"><a href="news-details.html">Luctus healthy food growth
                                     strategies required</a></h3>
                             <div class="news-one__bottom">
                                 <div class="news-one__read-more">
